fac4f2df6682f492f00da2325da7d2164c1a24c8
Jingyi Huang <reirawongg@gmail.com>
2024-04-09 21:54:59 +0900

    change the display elements and content in the About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,34 +7,42 @@ function About() {
             <div className="about">
                 <SideBar />
                 <div className="about_content">
-                    <div class="column-left">
-                        <img src={ require('../assets/images/profile/photo.jpg') } style={{ width: 500 }}/>
+                    <div class="section-left">
+                        <img src={ require('../assets/images/profile/photo.jpg') } style={{ width: 300 }}/>
                         <p>
                             Contact me @reirawongg@gmail.com <br></br>
                             for ML/CV projects :D
                         </p>
                     </div>
-                    <div class="column-right">
-                        <h3>Education</h3>
-                        <div>
-                            <dl>
-                            <dt>
-                                <span class="name">the University of Edinburgh</span>
-                                <span class="date">2018.09 - 2019.11</span>
-                            </dt>
-                            <dd>
-                                <span>M.S. of Stats with Data Science</span>
-                            </dd>
-                            <dt>
-                                <span class="name">Waseda University</span>
-                                <span class="date">2022.04 - 2024.03</span>
-                            </dt>
-                            <dd>M.S. of Computer Science 
-                                (Research on Computer Vision: <a href="https://esslab.jp/ja/">ess lab</a>)
-                            </dd>
-                            </dl>
+                    <div class="section-right">
+                        <h2>Education</h2>
+                        <div class="row">
+                            <h5 class="column name">the University of Edinburgh</h5>
+                            <h5 class="column loc">UK</h5>
+                            <h5 class="column date">2018.09 - 2019.11</h5>
                         </div>
-                        <h3>Work Experience</h3>
+                        <p>M.S. of Stats with Data Science</p>
+                        <div class="row">
+                            <h5 class="column name">Waseda University</h5>
+                            <h5 class="column loc">Tokyo</h5>
+                            <h5 class="column date">2022.04 - 2024.03</h5>
+                        </div>
+                        <p>M.S. of Computer Science 
+                                (Research on Computer Vision: <a href="https://esslab.jp/ja/">ess lab</a>)</p>
+                        <div class="segment"></div>
+                        <h2>Work Experience</h2>
+                        <div class="row">
+                            <h5 class="column name"><a href="https://corpy.co.jp/">Corpy & Co.</a></h5>
+                            <h5 class="column loc">Tokyo</h5>
+                            <h5 class="column date">2023.04 - 2024.03</h5>
+                        </div>
+                        <p>AI Engineer Intern</p>
+                        <div class="row">
+                            <h5 class="column name"><a href="https://ridge-i.com/">Ridge-i</a></h5>
+                            <h5 class="column loc">Tokyo</h5>
+                            <h5 class="column date">2023.04 - Present</h5>
+                        </div>
+                        <p>Incoming Machine Learning Engineer</p>
                     </div>
                 </div>
             </div>
